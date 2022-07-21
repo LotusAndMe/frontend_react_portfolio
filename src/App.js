e24c90b5e4a17c4afc77eece2cdd41afb6a70b94
LotusAndMe <60803449+LotusAndMe@users.noreply.github.com>
2022-07-22 00:44:36 +0300

    Implementing localization by react-intl

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 
 const App = () => {
+  const [locale, setLocale] = useState("en");
+  const handleChangeLocale = (e) => {
+    setLocale(e.target.value);
+  };
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar locale={locale} handleChange={handleChangeLocale} />
       <Header />
       <About />
       <Work />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,20 +4,36 @@ import { images } from '../../constants/'
 import { HiMenuAlt4, HiX } from 'react-icons/hi'
 import { motion } from 'framer-motion'
 import { Link } from 'react-scroll'
+import { IntlProvider, FormattedMessage, FormattedDate } from 'react-intl';
 
-const Navbar = () => {
+const message = {
+  en: {
+    mainManu: ['home', 'about', 'work', 'skills','testimonial', 'contact'],
+  },
+  ua: {
+    mainManu: ['домашня', 'про мене', 'роботи', 'навички','відгуки', 'звязок'],
+  },
+};
+
+const Navbar = ({locale,handleChange}) => {
   const [toggle, setToggle] = useState(false)
+
   
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
         <img src={images.logo} alt="logo" />
+         <select onChange={handleChange} defaultValue={locale}>
+        {['en', 'ua'].map((x) => (
+          <option key={x}>{x}</option>
+        ))}
+      </select>
       </div>
       <ul className='app__navbar-links'>
-        {['home', 'about', 'work', 'skills','testimonial', 'contact'].map((item) => (
+        {message[locale].mainManu.map((item,index) => (
           <li key={`link-${item}`} className='app__flex p-text'>
             <div />
-            <Link to={`${item}`} spy={true} smooth={true}>{item}</Link>
+            <Link to={`${message.en.mainManu[index]}`} spy={true} smooth={true}>{item}</Link>
           </li>
         ))}
       </ul>
@@ -33,9 +49,9 @@ const Navbar = () => {
             <HiX onClick={() => setToggle(false)} />
 
             <ul >
-              {['home', 'about', 'work', 'skills', 'testimonial','contact'].map((item) => (
+              {message[locale].mainManu.map((item,index) => (
                 <li key={item} >
-                  <Link to={`${item}`} spy={true} smooth={true} onClick={() => setToggle(false)}>{item}</Link>
+                  <Link to={`${message.en.mainManu[index]}`} spy={true} smooth={true} onClick={() => setToggle(false)}>{item}</Link>
                 </li>
               ))}
             </ul>
