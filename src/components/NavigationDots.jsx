f52a14ebb502f747851c9b1a315f8f3e220d74c3
LotusAndMe <60803449+LotusAndMe@users.noreply.github.com>
2022-07-21 22:03:36 +0300

    My own fix for styles footer

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll'
 const NavigationDots = ({ active }) => {
   return (
     <div className='app__navigation'>
-      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
+      {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
         <Link to={`${item}`} key={item + index} spy={true} smooth={true} className='app__navigation-dot' style={active === item ? { backgroundColor: '#313BAC' } : {}} />
       ))}
     </div>
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -45,11 +45,11 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+          <a href="mailto:myindexukrnet@gmail.com" className="p-text">Mail me: myindexukrnet@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+          <a href="tel:+380634909557" className="p-text">Phone me: +380634909557</a>
         </div>
       </div>
       {!isFormSubmitted ? (
